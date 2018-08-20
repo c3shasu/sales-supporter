dbe92158b91587dbef2fe209182091dd1b8820a0
c3syamada <yoshiyuki.yamada@c-cube.co.jp>
2018-08-20 10:40:55 +0900

    エンジニアに関するコミットです。

--- sales-supporter-web/src/main/java/jp/co/ccube/ss/controller/engineer/EngineerEditController.java
+++ sales-supporter-web/src/main/java/jp/co/ccube/ss/controller/engineer/EngineerEditController.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +13,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import jp.co.ccube.ss.controller.dispatch.DispatchEditController;
 import jp.co.ccube.ss.form.EngineerForm;
 import jp.co.ccube.ss.service.EngineerService;
 
@@ -19,10 +23,21 @@ import jp.co.ccube.ss.service.EngineerService;
 public class EngineerEditController {
 	@Autowired
 	EngineerService engineerService;
+	private static final Logger log = LoggerFactory.getLogger(DispatchEditController.class);
 	@RequestMapping(value = "/engineerEdit", method = RequestMethod.GET)
-		public String EngineerEdit(@ModelAttribute("form")@Valid  EngineerForm form, BindingResult result,Model model) throws ParseException {
+		public ModelAndView EngineerEdit(@ModelAttribute("form")@Valid  EngineerForm form, BindingResult result,ModelAndView mav,Model model) throws ParseException {
 
-		engineerService.editEngineerSearch(form);
-			return "engineer/engineerDetailEdit";
+		engineerService.engineerSearch(form);
+		if(form.getEmployeeNo()==null){
+			mav.setViewName("engineer/engineerBpEdit");}
+		mav.setViewName("engineer/engineerEmployeeEdit");
+		mav.addObject("ya", form);
+			return mav;
 	}
+	@RequestMapping(value = "/engineerEditConfirm", method = RequestMethod.POST)
+	public ModelAndView EngineerEditConfirm(@ModelAttribute("form")@Valid  EngineerForm form, BindingResult result,ModelAndView mav,Model model) throws ParseException {
+		engineerService.engineerUpdate(form);
+		mav.setViewName("engineer/engineerEditConfirm");
+		return mav;
+}
 }
