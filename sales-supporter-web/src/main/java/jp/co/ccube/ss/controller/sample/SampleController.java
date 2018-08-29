bc662c2d429494183c0412df96f74b2d011d3351
Your Name <you@example.com>
2018-08-29 14:55:41 +0900

    帳票出力のサンプルを作成

--- sales-supporter-web/src/main/java/jp/co/ccube/ss/controller/sample/SampleController.java
+++ sales-supporter-web/src/main/java/jp/co/ccube/ss/controller/sample/SampleController.java
@@ -1,5 +1,19 @@
 package jp.co.ccube.ss.controller.sample;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -57,12 +71,65 @@ public class SampleController extends AbstractController {
 	@RequestMapping(value = "/send", params = "send", method = RequestMethod.POST)
 	public String send(Model model, @RequestParam("param") String param) {
 		Users userInfo = usersDao.selectByPrimaryKey(param);
-		if(userInfo != null){
+		if (userInfo != null) {
 			model.addAttribute("result", userInfo.getName());
-		}else{
+		} else {
 			model.addAttribute("result", "");
 		}
 		return "sample/result";
+	}
+
+	@RequestMapping(value = "/createReportSample", method = RequestMethod.POST)
+	public String createReport(HttpServletResponse response, @RequestParam("param2") String param) {
+
+		// テンプレート取得
+		final String templatePath = "excel_templates/TestTemplate.xlsx";
+		Workbook workBook = getTemplate(templatePath);
+
+		// シート取得
+		Sheet sheet = workBook.getSheetAt(0);
+
+		// 値の埋め込み
+		sheet.getRow(2).getCell(1).setCellValue(param);
+
+		// ExcelファイルのByte化
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		try{
+			try {
+				workBook.write(bos);
+			} finally {
+			    bos.close();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		byte[] fileBytes = bos.toByteArray();
+
+		// レスポンスの設定
+		response.setContentType("application/octet-stream");
+		response.setHeader("Content-Disposition", "attachment; filename=" + "report.xlsx");
+		response.setContentLength(fileBytes.length);
+
+		// レスポンス書き出し
+		try (OutputStream os = response.getOutputStream()) {
+			os.write(fileBytes);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private Workbook getTemplate(String templatePath) {
+		Workbook workbook = null;
+		URL url = this.getClass().getClassLoader().getResource(templatePath);
+		if (url != null) {
+			try (InputStream is = new ByteArrayInputStream(Files.readAllBytes(Paths.get(url.toURI())));) {
+				workbook = WorkbookFactory.create(is);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return workbook;
 	}
 
 	/**
